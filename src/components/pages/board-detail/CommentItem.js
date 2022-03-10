ca466b60e906cc107ccfd03a63e7395f3135c482
rkdals213 <rkdals213@naver.com>
2022-03-10 20:51:35 +0900

    asd

--- src/components/pages/board-detail/CommentItem.js
+++ src/components/pages/board-detail/CommentItem.js
@@ -2,6 +2,10 @@ import classNames from "classnames"
 import styles from "./BoardDetail.module.css"
 import Description from "../../@common/Description/Description"
 import Button from "../../@common/button/Button"
+import {useState} from "react"
+import RegisterCommentForm from "./RegisterCommentForm"
+import * as Api from "../../../api"
+import useTokenContext from "../../../hooks/useTokenContext"
 
 const CommentItem = ({comment, className, ...props}) => {
     const id = comment.id
@@ -11,6 +15,33 @@ const CommentItem = ({comment, className, ...props}) => {
     const isWriter = comment.isWriter
     const createAt = comment.createdAt
     const modifiedAt = comment.modifiedAt
+
+    const [update, setUpdate] = useState(false)
+    const [writeComment, setWriteComment] = useState("")
+    const {token} = useTokenContext()
+
+    const handleUpdateButton = () => {
+        setUpdate(!update)
+    }
+
+    const registerComment = async () => {
+        await Api.updateComment(token, id, {
+            comment: writeComment
+        }).then(() => {
+            window.location.reload()
+        })
+    }
+
+    const handleCommentChange = ({target}) => {
+        setWriteComment(target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        registerComment(writeComment)
+    }
+
     return (
         <div className={classNames(styles["content-wrapper"], className)} {...props}>
             <div className={styles["text-container"]}>
@@ -18,13 +49,20 @@ const CommentItem = ({comment, className, ...props}) => {
                     <span>{writerName}</span>
                     <span>{content}</span>
                     {isWriter && (
-                        <Button>수정</Button>
+                        <Button onClick={handleUpdateButton}>수정</Button>
                     )}
                     <Description>등록일 : {createAt}</Description>
                     {createAt !== modifiedAt && (
                         <Description>수정일 : {modifiedAt}</Description>
                     )}
                 </div>
+                {update && (
+                    <RegisterCommentForm
+                        handleSubmit={handleSubmit}
+                        writeComment={writeComment}
+                        handleCommentChange={handleCommentChange}
+                    />
+                )}
             </div>
         </div>
     )
